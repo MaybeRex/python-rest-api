240e58ef4a652dd23ce7e4790cfd3bd540639a85
Mario Solorzano <marioxcore1@gmail.com>
2017-05-30 23:46:15 -0700

    Creating authentication tokens

--- src/electron_store/urls.py
+++ src/electron_store/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
+from rest_framework.authtoken import views as rest_framework_views
+
 from . import views
 
 from django.conf.urls import include
 
 urlpatterns = [
-    url(r'^register', views.UserRegisterAPIView.as_view()),
-    url(r'^login', views.UserLoginAPIView.as_view()),
+    url(r'^register/$', views.UserRegisterAPIView.as_view()),
+    url(r'^login/$', rest_framework_views.obtain_auth_token, name='login')
 ]
--- src/electron_store/views.py
+++ src/electron_store/views.py
@@ -1,11 +1,10 @@
-from django.shortcuts import render
-
 from rest_framework.views import APIView
 from rest_framework.generics import CreateAPIView
 from rest_framework.response import Response
 from rest_framework import status
+
 from rest_framework.authentication import TokenAuthentication
-from rest_framework import filters
+# from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.authtoken.views import ObtainAuthToken
@@ -35,6 +34,3 @@ class UserRegisterAPIView(CreateAPIView):
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-class UserLoginAPIView(APIView):
-    pass
